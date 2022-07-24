3681c8f2dbd0697e5006e2298b2d55ac1185123d
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-25 02:08:25 +0900

    クエリパラメーターがない時の処理を追加
    クエリパラメーターがない時にエラー表示がでない為の条件分岐をviews.pyのindex関数の定義部分に追加した。

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 
 def index(request): #HttpRequestクラスの引数requestを返すindex関数の定義
-  msg = request.GET['msg'] #HttpRequestクラスのGET属性(クエリパラメーターの値等の入っている辞書)を使って&msg=◯◯として送られた値を取り出している
-  return HttpResponse('you typed: "' + msg + '" .') #HttpResposeインスタンスを作成し、戻り値をHello Djangoとする
+  if 'msg' in request.GET: #GETの辞書の中にmsgというキーの値が保管されている時の条件分岐
+    msg = request.GET['msg'] #HttpRequestクラスのGET属性(クエリパラメーターの値等の入っている辞書)を使って&msg=◯◯として送られた値を取り出している
+    result = 'you typed: "' + msg + '" .' #変数resultに指定の文字列とパラメーターに記述した値を代入
+  else:
+    result = 'Please send msg parameter' #msgパラメーターを送信する旨を変数resultに代入
