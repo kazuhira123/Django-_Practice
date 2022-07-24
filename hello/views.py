8a5aba7ecd7eb6cfe479e32d9a3fd9b01242b4b4
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-24 23:19:53 +0900

    typo修正

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 
-def index(request): #HttpResponseインスタンスを実行するindex関数の定義
-  msg = request.GET['msg'] #HttpResponseクラスのGET属性(クエリパラメーターの値等の入っている辞書)を使って&msg=◯◯として送られた値を取り出している
+def index(request): #HttpRequestクラスの引数requestを返すindex関数の定義
+  msg = request.GET['msg'] #HttpRequestクラスのGET属性(クエリパラメーターの値等の入っている辞書)を使って&msg=◯◯として送られた値を取り出している
   return HttpResponse('you typed: "' + msg + '" .') #HttpResposeインスタンスを作成し、戻り値をHello Djangoとする
