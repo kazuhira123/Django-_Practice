c8314a25cd3183849cd920312b0f785db4596afd
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-24 23:09:10 +0900

    クエリパラメーターを使用した処理の追加
    request.GETを用いてurlの値の一部を画面上に表示する処理を追加した

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 
-def index(request): #HttpResponseを実行するindex関数の定義
-  return HttpResponse('Hello Django!!') #HttpResposeインスタンスを作成し、戻り値をHello Djangoとする
+def index(request): #HttpResponseインスタンスを実行するindex関数の定義
+  msg = request.GET['msg'] #HttpResponseクラスのGET属性(クエリパラメーターの値等の入っている辞書)を使って&msg=◯◯として送られた値を取り出している
+  return HttpResponse('you typed: "' + msg + '" .') #HttpResposeインスタンスを作成し、戻り値をHello Djangoとする
