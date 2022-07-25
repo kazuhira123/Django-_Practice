0e00034f7cee6341186c1fcfc50506239b831c84
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-25 23:26:29 +0900

    省略したクエリパラメーターでも画面表示できるように修正
    urlpatternsとindex関数を修正して、クエリパラメーターを省略した状態でもインタラクティブな表示ができるように修正した。

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,5 @@ from django.urls import path #django.urlsからpathモジュールのimport
 from . import views #カレントディレクトリのviewsモジュールのimport
 
 urlpatterns = [
-    path('', views.index, name = 'index'), #helloアプリケーションのアドレスを指定するため、アドレスは空白。引数nameに文字列indexを代入
+    path('<int:id>/<nickname>', views.index, name = 'index'), #アドレスの値をそれぞれid/nickname/という名前に設定し、引数nameに文字列indexを代入
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 
-def index(request): #HttpRequestクラスの引数requestを返すindex関数の定義
+def index(request, id, nickname): #HttpRequestクラスの引数requestを返すindex関数の定義、引数にurls.pyで定義したアドレスidとnicknameを追加
+  result = 'your id: ' + str(id) + ', name: "' \
+    + nickname + '" .' #idとnicknameに設定された値がresultに代入される
+  return HttpResponse(result)
+
+  '''
+  クエリパラメーターを使って表示を変更していた時の記述
   if 'msg' in request.GET: #GETの辞書の中にmsgというキーの値が保管されている時の条件分岐
     msg = request.GET['msg'] #HttpRequestクラスのGET属性(クエリパラメーターの値等の入っている辞書)を使って&msg=◯◯として送られた値を取り出している
     result = 'you typed: "' + msg + '" .' #変数resultに指定の文字列とパラメーターに記述した値を代入
   else:
     result = 'Please send msg parameter' #msgパラメーターを送信する旨を変数resultに代入
   return HttpResponse(result) #戻り値としてHttpResponseインスタンスの引数に変数resultを返す
+  '''
