29e7763984be786168ebf9d922d140757d4556d2
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-27 01:34:50 +0900

    index関数がテンプレート変数に値を渡す処理を追記
    index関数に辞書を追記し、テンプレート変数に値を渡す為の処理を作成した。

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 
 def index(request):
-  return render(request, 'hello/index.html') #テンプレートをレンダリングするrender関数の第2引数にtemplatesフォルダ内のパスを指定する
+  params = {
+    'title': 'Hello/Index', #テンプレート側で変数として定義した名前をキーとする辞書を作成
+    'msg': 'これは、サンプルで作ったページです'
+  }
+  return render(request, 'hello/index.html', params) #テンプレートをレンダリングするrender関数の第2引数にtemplatesフォルダ内のパス、第3引数にparamsに用意された値を戻し、レンダリング時に使用。
