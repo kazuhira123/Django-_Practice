0ab8f1d10a735c50ba696d574b0ba8c1e2daad1e
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-27 00:59:35 +0900

    urlpatternsとindexの修正
    テンプレート作成に合わせて、irlpatternsとindexを元の記述に戻した。

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,5 @@ from django.urls import path #django.urlsからpathモジュールのimport
 from . import views #カレントディレクトリのviewsモジュールのimport
 
 urlpatterns = [
-    path('<int:id>/<nickname>', views.index, name = 'index'), #アドレスの値をそれぞれid/nickname/という名前に設定し、引数nameに文字列indexを代入
+    path('', views.index, name='index'), #/hello/のアドレスでindexにアクセスできる形に戻した
 ]
