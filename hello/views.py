3f1d1f4e6c0ece93ad3f8fab194095bc318ead5a
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-28 02:56:50 +0900

    新規ページ用の要素&ビュー関数追加
    テンプレートに追加した新規ページ用の辞書のgoto要素とビュー関数nextを追記

--- hello/views.py
+++ hello/views.py
@@ -5,5 +5,14 @@ def index(request):
   params = {
     'title': 'Hello/Index', #テンプレート側で変数として定義した名前をキーとする辞書を作成
     'msg': 'これは、サンプルで作ったページです'
+    'goto': 'next', #リンクを作成したテンプレート変数gotoをキーとする辞書作成
   }
   return render(request, 'hello/index.html', params) #テンプレートをレンダリングするrender関数の第2引数にtemplatesフォルダ内のパス、第3引数にparamsに用意された値を戻し、レンダリング時に使用。
+
+def next(request): #テンプレートに新規作成したページ用のビュー関数nextを定義
+  params = {
+    'title': 'Hello/Next',
+    'msg': 'これは、２つめのページです',
+    'goto': 'index'
+  }
+  return render(request, 'hello/index', params)
