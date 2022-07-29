8fe3d583d0d8571c200db4a65a3c1350c72e76de
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-29 23:58:43 +0900

    ビュー関数indexにFormクラスを使用した処理を追加
    ビュー関数に条件分岐を追記することで、Formクラスを用いたフォームに値が入力されていたときの処理を追記した。

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
+from .forms import HelloForm #forms.pyのHelloFormクラスを呼び出す
 
 def index(request):
   params = {
-    'title':'Hello/Index', #テンプレート側で変数として定義した名前をキーとする辞書を作成
-    'msg':'これは、サンプルで作ったページです',
-    'goto':'next', #リンクを作成したテンプレート変数gotoをキーとする辞書作成
+    'title':'Hello', #テンプレート側で変数として定義した名前をキーとする辞書を作成
+    'msg':'your data',
+    'form':HelloForm(), #formをキーにHelloFormメソッドを呼び出す
   }
+  if (request.method == 'POST'): #フォームに値が入力したときの処理
+    params['massage'] = '名前：' + request.POST['name'] + \
+      '<br>メール' + request.POST['mail'] + \
+      '<br>年齢' +request.POST['age'] #paramsにmassageをキーとした要素を作成し、フォームに入力されたそれぞれの値を表示する。<br>は改行の意味
+    params['form'] = HelloForm(request.POST) #辞書paramsのformキーの要素の値をHelloFormクラスのrequest.POSTの値へ更新
   return render(request, 'hello/index.html', params) #テンプレートをレンダリングするrender関数の第2引数にtemplatesフォルダ内のパス、第3引数にparamsに用意された値を戻し、レンダリング時に使用。
 
+'''
 def next(request): #テンプレートに新規作成したページ用のビュー関数nextを定義
   params = {
     'title':'Hello/Next',
@@ -24,3 +31,4 @@ def form(request): #フォーム用のビュー関数formを定義
     'msg':'こんにちは、' + msg + 'さん。',
   }
   return render(request, 'hello/index.html', params)
+'''
