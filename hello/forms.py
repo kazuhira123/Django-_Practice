dd8ca2af3d0341c3e9a4efa1b1a6c82b72f9f73c
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-29 21:48:15 +0900

    DjangoのFormクラス用のスクリプトファイル作成
    DjangoのFormクラスを使用するためにforms.pyを新規作成した。さらに、その中にHelloFomrというクラスを定義し、フィールドを用意して変数へ代入した。

---
+++ hello/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
+  name = forms.CharField(label='name') #文字列のフィールドであるCharFieldを設定、引数にlabelを用意しそれぞれのフィールドの手前にテキストが表示されるようにしている
+  mail = forms.CharField(label='mail')
+  age = fomrs.IntegerField(label='age') #整数の値を入力するためのIntegerFiwldを設定
