f0b059ebbf81d0956d675117589e6fbb792fbb23
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-30 18:51:36 +0900

    フォーム部分をBootstrapでデザインした
    フォーム部分をBootstrapでデザインするためにforms.pyにウィジェットを追加した

--- hello/forms.py
+++ hello/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
-  name = forms.CharField(label='name') #文字列のフィールドであるCharFieldを設定、引数にlabelを用意しそれぞれのフィールドの手前にテキストが表示されるようにしている
-  mail = forms.CharField(label='mail')
-  age = forms.IntegerField(label='age') #整数の値を入力するためのIntegerFiwldを設定
+  name = forms.CharField(label='name', \
+    widget=forms.TextInput(attrs={'class':'form-control'})) #文字列のフィールドであるCharFieldを設定、引数にlabelを用意しそれぞれのフィールドの手前にテキストが表示されるようにしている
+  mail = forms.CharField(label='mail', \
+    widget=forms.TextInput(attrs={'class':'form-control'})) #CharFieldのインスタンス生成時に引数にwidgetを用意してウィジェットクラスを作成している。
+  age = forms.IntegerField(label='age', \
+    widget=forms.NumberInput(attrs={'class':'form-control'})) #整数の値を入力するためのIntegerFiwldを設定
