1c5605bc709ba1699108dcc676c73280b982ab32
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-31 01:36:29 +0900

    view関数のクラス化
    ビュー関数indexを消して、新たにビューを表示する処理を記述したHelloViewクラスを定義することで、GETメソッドとPOSTメソッドのアクセスによる処理を1つのクラスで行えるようになった

--- hello/views.py
+++ hello/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
+from django.views.generic import TemplateView #TemplateViewクラスを呼び出す
 from .forms import HelloForm #forms.pyのHelloFormクラスを呼び出す
 
-def index(request):
-  params = {
-    'title':'Hello', #テンプレート側で変数として定義した名前をキーとする辞書を作成
-    'massage':'your data',
-    'form':HelloForm() #formをキーにHelloFormメソッドを呼び出す
-  }
-  if (request.method == 'POST'): #フォームに値が入力したときの処理
-    params['massage'] = '名前：' + request.POST['name'] + \
-      '<br>メール' + request.POST['mail'] + \
-      '<br>年齢' +request.POST['age'] #paramsにmassageをキーとした要素を作成し、フォームに入力されたそれぞれの値を表示する。<br>は改行の意味
-    params['form'] = HelloForm(request.POST) #辞書paramsのformキーの要素の値をHelloFormクラスのrequest.POSTの値へ更新
-  return render(request, 'hello/index.html', params) #テンプレートをレンダリングするrender関数の第2引数にtemplatesフォルダ内のパス、第3引数にparamsに用意された値を戻し、レンダリング時に使用。
+class HelloView(TemplateView):
+  def __init__(self):
+    self.params = {
+      'title':'Hello', #テンプレート側で変数として定義した名前をキーとする辞書を作成
+      'massage':'your data',
+      'form':HelloForm() #formをキーにHelloFormメソッドを呼び出す
+    }
+
+  def get(self, request): #GETメソッドで値が渡された時の処理
+    return render(request, 'hello/index.html', self.params) #レンダリング時にself.paramsを戻り値として返すだけの処理
+
+  def post(self, request): #POSTメソッドで値が渡された時の処理
+    msg = 'あなたは、<b>' + request.POST['name'] + \
+      '(' + request.POST['age'] + \
+      ') </b>さんです。<br>メールアドレスは<b>' + request.POST['mail'] + \
+      '</b> ですね。' #フォームで入力した値をbタグで太字にして表示する値を変数msgへ代入
+    self.params['massage'] = msg #self.paramsのキーがmassageの要素の中身を変数msgの値にへ更新
+    self.params['form'] = HelloForm(request.POST) #入力した値をクリアさせないための処理
+    return render(request, 'hello/index.html', self.params)
