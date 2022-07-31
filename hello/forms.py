20ec0d51da7799af7ba81203185b0fb7a90bf52a
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-31 21:37:05 +0900

    チェックボックス用のインスタンス生成
    チェックボックス表示用のBooleanFieldを元にcheckインスタンスを生成した。

--- hello/forms.py
+++ hello/forms.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from django import forms
 
 class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
@@ -7,3 +8,4 @@ class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
     widget=forms.TextInput(attrs={'class':'form-control'})) #CharFieldのインスタンス生成時に引数にwidgetを用意してウィジェットクラスを作成している。
   age = forms.IntegerField(label='age', \
     widget=forms.NumberInput(attrs={'class':'form-control'})) #整数の値を入力するためのIntegerFiwldを設定
+  check = forms.BooleanField(label='Checkbox', required=False) #チェックボックス用のクラスBooleanFieldを元にcheckインスタンスを生成
