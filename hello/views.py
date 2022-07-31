8babc17b8ce5ff822d89389a30181e5b6308e28b
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-07-31 23:14:47 +0900

    ビュークラスにチェックボックス用の条件分岐を記述
    ビュークラス内にチェックボックスのON/OFFに合わせた条件分岐の処理を追記

--- hello/views.py
+++ hello/views.py
@@ -8,13 +8,18 @@ class HelloView(TemplateView):
     self.params = {
       'title':'Hello', #テンプレート側で変数として定義した名前をキーとする辞書を作成
       'massage':'your data',
-      'form':HelloForm() #formをキーにHelloFormメソッドを呼び出す
+      'form':HelloForm(), #formをキーにHelloFormメソッドを呼び出す
+      'result':None
     }
 
   def get(self, request): #GETメソッドで値が渡された時の処理
     return render(request, 'hello/index.html', self.params) #レンダリング時にself.paramsを戻り値として返すだけの処理
 
   def post(self, request): #POSTメソッドで値が渡された時の処理
+    if ('check' in request.POST): #チェックボックスの値がTrueの時の処理
+      self.params['result'] = 'Checked!'
+    else: #チェックボックスがFalseの時の処理、チェックボックスがOFFの時は値が送信されないのでelse文で記述可能
+      self.params['result'] = 'not checked...'
     msg = 'あなたは、<b>' + request.POST['name'] + \
       '(' + request.POST['age'] + \
       ') </b>さんです。<br>メールアドレスは<b>' + request.POST['mail'] + \
