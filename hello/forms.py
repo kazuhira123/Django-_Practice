df1c7b7ed16ba19080a84e38703d798df85b82ec
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-01 21:56:30 +0900

    プルダウン用のFormクラスの作成
    プルダウン用のクラスであるChoiceFieldクラスを継承するchoiceインスタンスを生成

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,10 @@
+from secrets import choice
 from django import forms
 
 class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
-  check = forms.NullBooleanField(label='check') #プルダウン用のクラスNullBooleanFieldクラスを元にcheckインスタンスを生成
+  data=[ #変数dataにタプル型の値をリスト化して代入する
+    ('one', 'item 1'),
+    ('two', 'item 2'),
+    ('three', 'item 3')
+  ]
+  choice = forms.ChoiceField(label='Choice', choices=data) #引数choicesの値にdataを代入
