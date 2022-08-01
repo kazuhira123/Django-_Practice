491382f49f2917739982831ae9fc43d89fc19564
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-01 23:35:29 +0900

    ラジオボタン用のFormクラスを定義
    ラジオボタン作成用のクラスを作成する為、ChoiceFieldクラスのwidget引数にRadioSelectクラスのインスタンスを作成した。

--- hello/forms.py
+++ hello/forms.py
@@ -7,4 +7,5 @@ class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
     ('two', 'item 2'),
     ('three', 'item 3')
   ]
-  choice = forms.ChoiceField(label='Choice', choices=data) #引数choicesの値にdataを代入したchoiceインスタンスを生成
+  choice = forms.ChoiceField(label='Choice',\
+    choices=data, widget=forms.RadioSelect) #引数choicesの値にdataを代入したchoiceインスタンスを生成、さらにwidget引数にRadioSelectのインスタンスを作成してラジオボタンを作成できるようにした
