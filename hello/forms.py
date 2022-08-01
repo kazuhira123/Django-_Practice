11b2b51acfb83b1225ec6499b42260182d863cc2
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-01 21:38:42 +0900

    プルダウンメニューの作成
    NullBooleanFieldを用いたプルダウンメニューの作成が完了した。(うまくいかなかった原因はよくわからず)

--- hello/forms.py
+++ hello/forms.py
@@ -1,11 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
-  name = forms.CharField(label='name', \
-    widget=forms.TextInput(attrs={'class':'form-control'})) #文字列のフィールドであるCharFieldを設定、引数にlabelを用意しそれぞれのフィールドの手前にテキストが表示されるようにしている
-  mail = forms.CharField(label='mail', \
-    widget=forms.TextInput(attrs={'class':'form-control'})) #CharFieldのインスタンス生成時に引数にwidgetを用意してウィジェットクラスを作成している。
-  age = forms.IntegerField(label='age', \
-    widget=forms.NumberInput(attrs={'class':'form-control'})) #整数の値を入力するためのIntegerFiwldを設定
-  check = forms.BooleanField(label='Checkbox', required=False) #チェックボックス用のクラスBooleanFieldを元にcheckインスタンスを生成
-  select = forms.NullBooleanField(label='Select') #プルダウン用のクラスNullBooleanFieldクラスを元にselectインスタンスを生成
+  check = forms.NullBooleanField(label='check') #プルダウン用のクラスNullBooleanFieldクラスを元にcheckインスタンスを生成
--- hello/views.py
+++ hello/views.py
@@ -10,25 +10,15 @@ class HelloView(TemplateView):
     self.params = {
       'title':'Hello', #テンプレート側で変数として定義した名前をキーとする辞書を作成
       'massage':'your data',
-      'form':HelloForm(), #formをキーにHelloFormメソッドを呼び出す
-      'check_result':None,
-      'select_result':None,
+      'result':None,
+      'form':HelloForm(),
     }
 
   def get(self, request): #GETメソッドで値が渡された時の処理
     return render(request, 'hello/index.html', self.params) #レンダリング時にself.paramsを戻り値として返すだけの処理
 
   def post(self, request): #POSTメソッドで値が渡された時の処理
-    if ('check' in request.POST): #チェックボックスの値がTrueの時の処理
-      self.params['check_result'] = 'Checked!'
-    else: #チェックボックスがFalseの時の処理、チェックボックスがOFFの時は値が送信されないのでelse文で記述可能
-      self.params['check_result'] = 'not checked...'
-    msg = 'あなたは、<b>' + request.POST['name'] + \
-      '(' + request.POST['age'] + \
-      ') </b>さんです。<br>メールアドレスは<b>' + request.POST['mail'] + \
-      '</b> ですね。' #フォームで入力した値をbタグで太字にして表示する値を変数msgへ代入
-    slct = request.POST['select']
-    self.params['select_result'] = 'you selected: "' + slct + '"'
-    self.params['massage'] = msg #self.paramsのキーがmassageの要素の中身を変数msgの値にへ更新
-    self.params['form'] = HelloForm(request.POST) #入力した値をクリアさせないための処理
+    chk = request.POST['check']
+    self.params['result'] = 'you selected: "' + chk + '"'
+    self.params['form'] = HelloForm(request.POST)
     return render(request, 'hello/index.html', self.params)
