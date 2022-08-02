671582ef878df0910879876d52e3699ac3091753
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-02 17:18:28 +0900

    Friendモデルの定義
    データベースのテーブル作成用にモデルをhelloアプリケーションのmodels.pyに定義した。

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from unicodedata import name
+from django.db import models #django.dbモジュールから、modelsパッケージをimportする
 
-# Create your models here.
+class Friend(models.Model): #Friendクラスの定義、下記に定義された情報がテーブルの項目と入れる値のデータ型を決定する
+  name = models.CharField(max_length=100) #各項目を変数にレコードの定義をインスタンスに記述
+  mail = models.EmailField(max_length=200)
+  gender = models.BooleanField()
+  age = models.IntegerField(default=0)
+  birthday = models.DateField()
+
+  def __str__(self): #テキストの値を返す__str__メソッドを定義、これによってFriendクラスのインスタンスを{{}}で表示可能に
+      return '<Friend:id=' + str(self.id) + ',' + self.name + '(' + str(self.age) + ')>'
+
