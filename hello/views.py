a0faa833be4fd909394ad67fe58c1f2ecb0543a4
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-02 23:53:20 +0900

    views.pyにレコードを取得する処理を記述
    views.pyにFriendクラスをimportし、関数index内にレコードを取得するobject.allメソッドを記述した

--- hello/views.py
+++ hello/views.py
@@ -4,20 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 from django.views.generic import TemplateView #TemplateViewクラスを呼び出す
 from .forms import HelloForm #forms.pyのHelloFormクラスを呼び出す
+from .models import Friend
 
-class HelloView(TemplateView):
-  def __init__(self):
-    self.params = {
-      'title':'Hello', #テンプレート側で変数として定義した名前をキーとする辞書を作成
-      'form':HelloForm(),
-      'result':None,
-    }
-
-  def get(self, request): #GETメソッドで値が渡された時の処理
-    return render(request, 'hello/index.html', self.params) #レンダリング時にself.paramsを戻り値として返すだけの処理
-
-  def post(self, request): #POSTメソッドで値が渡された時の処理
-    self.params['form'] = HelloForm(request.POST)
-    ch = request.POST.getlist('choice') #変数chにリストの値を取得するgetlistメソッドで取得した値を代入
-    self.params['result'] = 'you selected: "' + str(ch) + '"' #リストから取得した値を文字列型のデータに変換して表示するようにresultキーの要素を更新
-    return render(request, 'hello/index.html', self.params)
+def index(request):
+  data = Friend.objects.all() #Friendクラスのobject属性のallメソッドを利用して、レコードを全て取得する
+  params = {
+    'title': 'Hello',
+    'massage': 'all friend.',
+    'data': data,
+  }
+  return render(request, 'hello/index.html', params)
