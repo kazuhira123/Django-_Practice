89e944b66fe2edc43e23eb59c25ab84bb9ea7763
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-05 00:54:48 +0900

    テンプレートにレコードから取得した値を表示する処理をビュー側に追記
    テンプレート上にレコードから取得した値をテーブル表示する処理を、ビュー側で処理するように修正

--- hello/views.py
+++ hello/views.py
@@ -1,25 +1,28 @@
 from tkinter.messagebox import NO
 from turtle import pd
+from unittest import result
 from django.shortcuts import render
 from django.http import HttpResponse #HttpResponseクラスをimportする
 from django.views.generic import TemplateView #TemplateViewクラスを呼び出す
 from .forms import HelloForm #forms.pyのHelloFormクラスを呼び出す
 from .models import Friend
+from django.db.models import QuerySet
+
+def __new_str__(self): #表示するレコードの値を更新する為の関数__new_str__を定義
+  result = ''
+  for item in self:
+    result += '<tr>' #ヘッダー用のタグを追加
+    for k in item:
+      result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+    result += '</tr>'
+  return result
+
+QuerySet.__str__ = __new_str__ #モデル上で定義した__str__の値を__new_str__に更新する
 
 def index(request):
-  data = Friend.objects.all() #Friendクラスのobject属性のallメソッドを利用して、レコードを全て取得する
+  data = Friend.objects.all().values('id', 'name', 'age') #Friendクラスのobject属性のallメソッドを利用してレコードを全て取得し、valuesメソッドで取得したレコード内の任意のキーの値を返す
   params = {
     'title': 'Hello',
-    'massage': 'all friend.',
-    'form':HelloForm(),
-    'data': [],
+    'data': data, #Friendオブジェクトから取得した値がparamsのdata要素に入る
   }
-  if (request.method == 'POST'):
-    num=request.POST['id'] #変数numにPOSTメソッドで送信された時のidの値を代入する
-    item = Friend.objects.get(id=num) #IDの値がnumと一致するレコードの値を取り出す
-    params['data'] = [item] #テンプレートで繰り返し処理を実施するitemの値をparamsのdata要素に代入する
-    params['form'] = HelloForm(request.POST)
-  else:
-    params['data'] = Friend.objects.all() #HTTPメソッドがPOSTでない時、Friendモデル内の全ての値をparamsのdata要素に代入
-
   return render(request, 'hello/index.html', params)
