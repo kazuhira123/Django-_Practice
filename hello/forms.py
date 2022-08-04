e1c22b6e1ddec1908acf0efa6bf811483e42e994
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-04 23:17:06 +0900

    レコード取り出し用のフォーム作成
    レコードのIDを取り出す用に数値入力型のフォーム作成

--- hello/forms.py
+++ hello/forms.py
@@ -2,10 +2,4 @@ from secrets import choice
 from django import forms
 
 class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
-  data=[ #変数dataにタプル型の値をリスト化して代入する
-    ('one', 'item 1'),
-    ('two', 'item 2'),
-    ('three', 'item 3')
-  ]
-  choice = forms.MultipleChoiceField(label='radio', choices=data, \
-    widget=forms.SelectMultiple(attrs={'size':3})) #引数choicesの値にdataを代入したchoiceインスタンスを生成、さらにwidget引数にSelectのインスタンスを作成して選択リストを作成できるようにした
+  id = forms.IntegerField(label='ID')
