9c52d1d723164cd410456d54f1a3a138f2fb341c
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-04 23:39:37 +0900

    指定のIDのレコードを取り出すビューの処理をviews.pyに追加
    追加したフォームを利用した指定のレコードを取り出す処理をobjects.getメソッドで実装

--- hello/views.py
+++ hello/views.py
@@ -11,6 +11,15 @@ def index(request):
   params = {
     'title': 'Hello',
     'massage': 'all friend.',
-    'data': data,
+    'form':HelloForm(),
+    'data': [],
   }
+  if (request.method == 'POST'):
+    num=request.POST['id'] #変数numにPOSTメソッドで送信された時のidの値を代入する
+    item = Friend.objects.get(id=num) #IDの値がnumと一致するレコードの値を取り出す
+    params['data'] = [item] #テンプレートで繰り返し処理を実施するitemの値をparamsのdata要素に代入する
+    params['form'] = HelloForm(request.POST)
+  else:
+    params['data'] = Friend.objects.all() #HTTPメソッドがPOSTでない時、Friendモデル内の全ての値をparamsのdata要素に代入
+
   return render(request, 'hello/index.html', params)
