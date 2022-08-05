234f48fd9b29e58b57de16fafee419d04579655c
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-05 23:50:38 +0900

    Create実装用のフォームコントロールを新規作成
    Create学習用のフォームをforms.py上に、HelloFormクラスを継承して作成

--- hello/forms.py
+++ hello/forms.py
@@ -1,5 +1,11 @@
+from re import A
 from secrets import choice
+from unicodedata import name
 from django import forms
 
 class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
-  id = forms.IntegerField(label='ID')
+  name = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'form-control'})) #フォームの項目を追加し、それぞれのwidget引数にBootstrapのクラスを設定
+  mail = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class':'form-control'}))
+  gender = forms.BooleanField(label='Gender', required=False, widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+  age = forms.IntegerField(label='Age', widget=forms.NumberInput(attrs={'class':'from-control'}))
+  birthday = forms.DateField(label='Birth', widget=forms.DateInput(attrs={'class':'form-control'}))
