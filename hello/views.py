34add5705ff733e3611f541613d2027c27e45b02
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-06 00:31:52 +0900

    実装したcreate用のビュー関数作成
    新規作成したcreate.htmlを表示するためのビュー関数createを新規作成した

--- hello/views.py
+++ hello/views.py
@@ -1,28 +1,36 @@
+import re
 from tkinter.messagebox import NO
 from turtle import pd
+from unicodedata import name
 from unittest import result
-from django.shortcuts import render
+from django.shortcuts import redirect, render #redirectとrender関数をimport
 from django.http import HttpResponse #HttpResponseクラスをimportする
 from django.views.generic import TemplateView #TemplateViewクラスを呼び出す
 from .forms import HelloForm #forms.pyのHelloFormクラスを呼び出す
 from .models import Friend
 from django.db.models import QuerySet
 
-def __new_str__(self): #表示するレコードの値を更新する為の関数__new_str__を定義
-  result = ''
-  for item in self:
-    result += '<tr>' #変数resultにテーブルの横軸の要素を追加(テーブルの各項目が入るイメージ)
-    for k in item:
-      result += '<td>' + str(k) + ' = ' + str(item[k]) + '</td>' #変数resultに縦軸の要素として、「辞書のキー=キーに紐づく要素の中身」の形で値を代入、取り出す値はindexで指定したレコードとなる
-    result += '</tr>'
-  return result
-
-QuerySet.__str__ = __new_str__ #QuerySetの__str__を__new_str__に更新する
-
 def index(request):
-  data = Friend.objects.all().values('id', 'name', 'age') #Friendクラスのobject属性のallメソッドを利用してレコードを全て取得し、valuesメソッドで取得したレコード内の任意のキーの値を返す。ここでのallやvaluesメソッドの動きは↑で更新したQuerySetの通りに表示される
+  data = Friend.objects.all()
   params = {
     'title': 'Hello',
     'data': data, #Friendオブジェクトから取得した値がparamsのdata要素に入る
   }
   return render(request, 'hello/index.html', params)
+
+#create model
+def create(request):
+  params = {
+    'title': 'Hello',
+    'data': data, #Friendオブジェクトから取得した値がparamsのdata要素に入る
+  }
+  if (request.method == 'POST'):
+    name = request.POTS['name'] #ここから変数にPOSTメソッドで送信されたテーブルの各項目の値を代入する処理
+    mail = request.POST['mail']
+    gender = 'gender' in request.POST #BooleanFieldで定義されているgender要素だけ処理が違う(POSTメソッドで送信された値からgenderキーの要素を取り出している)
+    age = int(request.POST['age']) #ageキーの要素を整数として取り出す
+    birth = request.POST['birthday']
+    friend = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth) #上記で定義した変数の値を元にFriendインスタンスを生成
+    friend.save() #firendインスタンスの保存
+    return redirect(to='/hello') #上記インスタンスを保存した後、/helloにリダイレクト(自動的に他のWebページに転送されること)される
+  return render(request, 'hello/creste.html', params) #create.htmlを表示する際にcreate関数を返す
