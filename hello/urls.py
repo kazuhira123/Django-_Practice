a644de05c492a48b791abdbbbaeff771f77ef35d
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-07 01:25:08 +0900

    ジェネリックビュー表示用のアドレスを登録

--- hello/urls.py
+++ hello/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url #django.conf.urlsからurlモジュールのimport
 from . import views #viewsをimportする
+from .views import FriendList
+from .views import FriendDetail
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create', views.create, name='create'),
     path('edit/<int:num>', views.edit, name='edit'), #/edit/の後ろにIDの数字を入れることができるように指定
-    path('delete/<int:num>', views.delete, name='delete')
+    path('delete/<int:num>', views.delete, name='delete'),
+    path('list', FriendList.as_view()), #FriendListをビュークラスであるViewインスタンスとして取り出す
+    path('detail/<int:pk>', FriendDetail.as_view()), #☆
   ]
