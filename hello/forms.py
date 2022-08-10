39fea5e2ddf3655ff9121fbb94a482d5ed4d38f6
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-11 00:44:51 +0900

    CheckFormにcleanメソッドを定義し、自作のバリデーションを追加した
    super().clean()とValidationErrorを利用して、任意の値が入力されて時にエラーメッセージを出力するバリデーションを作成した

--- hello/forms.py
+++ hello/forms.py
@@ -2,6 +2,7 @@ import imp
 from re import A
 from secrets import choice
 from unicodedata import name
+from xml.dom import ValidationErr
 from django import forms
 from .models import Friend
 
@@ -13,10 +14,15 @@ class HelloForm(forms.Form): #HelloFormクラスにforms.Formクラスを継承
   birthday = forms.DateField(label='Birth', widget=forms.DateInput(attrs={'class':'form-control'}))
 
 class CheckForm(forms.Form):
-  str = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class':'form-control'}))
+  str = forms.CharField(label='String', widget=forms.TextInput(attrs={'class':'form-control'}))
   Empty = forms.CharField(label='Empty', empty_value=True, widget=forms.TextInput(attrs={'class':'form-control'}))
   min = forms.CharField(label='Min', min_length=5, widget=forms.TextInput(attrs={'class':'form-control'}))
   max = forms.IntegerField(label='Max', max_value=10000, widget=forms.NumberInput(attrs={'class':'form-control'}))
+  def clean(self):
+    cleaned_data = super().clean() #CheckFomrに入力された値を検証する
+    str = cleaned_data['str']
+    if (str.lower().startswith('no')):
+      raise forms.ValidationError('You input "NO"!') #ValidationErrクラスによってバリデーションのエラーを表示
 
 class FriendForm(forms.ModelForm): #ModelFormクラスを継承したFriendFormクラスを定義
   class Meta: #ModelFormの内部クラスMetaの定義(メタクラスと呼ばれるらしい)
