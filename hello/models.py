a7e7897ab1f32d4384bf34f082d3a30674f343b2
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-12 23:07:28 +0900

    モデルクラスのバリデーションを新規で設定
    モデルに設定した項目の引数にvalidatorsというリストを設定しいうリストを設定し、バリデーターを設定

--- hello/models.py
+++ hello/models.py
@@ -1,11 +1,14 @@
 from unicodedata import name
 from django.db import models #django.dbモジュールから、modelsパッケージをimportする
+from django.core.validators import MinValueValidator, MaxValueValidator #最小値と最大値設定用のクラスをimport
 
 class Friend(models.Model): #Friendクラスの定義、下記に定義された情報がテーブルの項目と入れる値のデータ型を決定する
   name = models.CharField(max_length=100) #各項目を変数にレコードの定義をインスタンスに記述
   mail = models.EmailField(max_length=200)
   gender = models.BooleanField()
-  age = models.IntegerField(default=0)
+  age = models.IntegerField(validators=[ \
+    MinValueValidator(0), \
+    MaxValueValidator(150)]) #項目ageに最小値と最大値のバリデーションを設定
   birthday = models.DateField()
 
   def __str__(self): #テキストの値を返す__str__メソッドを定義、これによってFriendクラスのインスタンスを{{}}で表示可能に
