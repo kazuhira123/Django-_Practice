126b1fde76a96b43953dd0298f23f60b03a44e00
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-12 23:55:29 +0900

    バリデーター関数を定義し、モデルのバリデーションに指定

--- hello/models.py
+++ hello/models.py
@@ -1,10 +1,19 @@
-from unicodedata import name
+import re #Pythonの正規表現モジュールをimport
+from sqlite3 import paramstyle
 from django.db import models #django.dbモジュールから、modelsパッケージをimportする
 from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator #モデルルール用のバリデーターをimport
+from django.core.validators import ValidationError
+
+def number_only(value):
+  if (re.match(r'^[0-9]*$', value) == None): #reモジュールのmatch関数を用いて入力されたテキストが指定のパターンと合致するかチェック
+    raise ValidationError(
+      '%(value)s is not Number!', \
+      params={'value':value}
+    )
 
 class Friend(models.Model): #Friendクラスの定義、下記に定義された情報がテーブルの項目と入れる値のデータ型を決定する
   name = models.CharField(max_length=100, \
-    validators=[RegexValidator(r'^[a-z]*$')]) #正規表現を利用して、nameにa~zまでの小文字だけを入力できるように設定 
+    validators=[number_only]) #自分で定義したバリデーター用の関数をバリデーションとして設定
   mail = models.EmailField(max_length=200)
   gender = models.BooleanField()
   age = models.IntegerField(validators=[ \
