c2e4b2ca3ac03da768ac390b2ece28932cfde127
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-15 00:56:31 +0900

    新規モデルのマイグレーション実行と管理ツールへの登録
    ターミナルへのコマンドとadmin.pyの記述を修正し、新規モデルのマイグレーション実行と管理ツールへ登録を実施

--- hello/admin.py
+++ hello/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Friend #Friendモデルのimport
+from .models import Friend, Message #FriendモデルとMessageモデルのimport
 
 admin.site.register(Friend) #Friendモデルを管理ツールに登録する
+admin.site.register(Message) #Messageモデルを管理ツールに登録する
