247966f82520c5511d1a8ae6d4a93bf713c2da32
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-18 20:19:41 +0900

    新規アプリケーション作成

--- django_app/settings.py
+++ django_app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'     #☆
+    'hello',     #☆
+    'sns' #練習用snsアプリ
 ]
 
 MIDDLEWARE = [
