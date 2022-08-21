f6b209d5b251139e8660bf96dcb5d573d1228487
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-08-22 01:03:20 +0900

    シェア機能処理用のビュー関数shareを追加した

--- sns/views.py
+++ sns/views.py
@@ -1,6 +1,8 @@
 #外部のメソッドとクラスのimport
 import re
 from sqlite3 import paramstyle
+from sre_constants import GROUPREF
+from tkinter.messagebox import NO
 from turtle import fd, title
 from django import shortcuts
 from django.shortcuts import render
@@ -193,3 +195,40 @@ def post(request):
   }
   return render(request, 'sns/post.html', params)
 
+#投稿をシェアする
+@login_required(login_url='/admin/login/')
+def share(request, share_id):
+  #シェアするMessageの取得
+  share = Message.objects.get(id=share_id)
+  print(share)
+  #POST送信時の処理
+  if request.method == 'POST':
+    #送信内容を取得
+    gr_name = request.POST['groups']
+    content = request.POST['content']
+    #Groupの取得
+    group = Group.objects.filter(owner=request.user).filter(title=gr_name).first()
+    if group == None:
+      (pub_user, group) = get_public()
+    #メッセージを作成し、設定し保存
+    msg = Message()
+    msg.owner = request.user
+    msg.group = group
+    msg.content = content
+    msg.share_id = share_id
+    msg.save()
+    share_msg = msg.get_share()
+    share_msg.share_count += 1
+    share_msg.save()
+    #メッセージを設定
+    messages.success(request, 'メッセージをシェアしました！')
+    return redirect(to='/sns')
+  
+  #共通処理
+  form = PostForm(request, user)
+  params = {
+    'login_user':request.user,
+    'form':form,
+    'share':share,
+  }
+  return render(request, 'sns/share.html', params)
