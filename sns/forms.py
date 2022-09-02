d10789742055ee53c94485d1cc011231dd7be837
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-09-03 02:59:37 +0900

    forms.pyの修正
    各フォームのchoices変数の['-', '-']の記述を[('-', '-')]に修正

--- sns/forms.py
+++ sns/forms.py
@@ -46,7 +46,7 @@ class GroupSelectForm(forms.Form):
   def __init__(self, user, *args, **kwargs):
     super(GroupSelectForm, self).__init__(*args, **kwargs)
     self.fields['groups'] = forms.ChoiceField(
-      choices=['-', '-'] + [(item.title, item.title) \
+      choices=[('-', '-')] + [(item.title, item.title) \
         for item in Group.objects.filter(owner=user)],
         widget=forms.Select(attrs={'class':'form-control'}),
     )
@@ -64,7 +64,7 @@ class FriendsForm(forms.Form):
 #Group作成フォーム
 class CreateGroupForm(forms.Form):
   group_name = forms.CharField(max_length=50, \
-    widget=forms.TextInput(attrs={'class':'formcontrol'}))
+    widget=forms.TextInput(attrs={'class':'form-control'}))
 
 #投稿フォーム
 class PostForm(forms.Form):
@@ -75,7 +75,8 @@ class PostForm(forms.Form):
     super(PostForm, self).__init__(*args, **kwargs)
     public = User.objects.filter(username='public').first()
     self.fields['groups'] = forms.ChoiceField(
-      choices=['-', '-'] + [(item.title, item.title) \
-        for item in Group.objects.filter(owner__in=[user, public])],
+      choices=[('-', '-')] + [(item.title, item.title) \
+        for item in Group.objects. \
+          filter(owner__in=[user, public])],
         widget=forms.Select(attrs={'class':'form-control'}),
     )
