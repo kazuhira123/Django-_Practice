06d56f9dc3ed8a97751644c0662cbf83196201fb
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-09-03 02:55:21 +0900

    models.pyの修正
    FriendクラスのgroupインスタンスのForeingKeyメソッド内のGroupが文字列になっていたのを修正

--- sns/models.py
+++ sns/models.py
@@ -36,7 +36,7 @@ class Friend(models.Model):
   owner = models.ForeignKey(User, on_delete=models.CASCADE, \
     related_name='friend_owner')
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  group = models.ForeignKey('Group', on_delete=models.CASCADE)
+  group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
   def __str__(self):
       return str(self.user) + '(group:"' + str(self.group) + '")'
@@ -46,4 +46,5 @@ class Good(models.Model):
   message = models.ForeignKey(Message, on_delete=models.CASCADE)
 
   def __str__(self):
-      return 'good for "' + str(self.message) + '" (by ' + str(self.owner) + ')'
+      return 'good for "' + str(self.message) + '" (by ' + \
+        str(self.owner) + ')'
